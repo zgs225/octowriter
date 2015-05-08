e6788af9566524e43cdefcb1913ebd88daeb5bdb
zgs225 <zgs225@gmail.com>
2015-05-08 10:58:43 +0800

    Initial Commit

---
+++ octowriter/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  octowriter
+//
+//  Created by Yuez on 15/5/8.
+//  Copyright (c) 2015年 Yuez. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ octowriterTests/octowriterTests.swift
@@ -0,0 +1,36 @@
+//
+//  octowriterTests.swift
+//  octowriterTests
+//
+//  Created by Yuez on 15/5/8.
+//  Copyright (c) 2015年 Yuez. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class octowriterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
